07fd7a3f50b9338211f1eb24d4629dae43b2058e
ColShark <petre.marius93@gmail.com>
2023-01-14 13:37:25 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	for (int numar = 0; numar <= 5; numar++)
+	{
+		if (numar == 4)
+		{
+			break;
+		}
+
+		std::cout << numar << std::endl;
+
+	}
+	system("pause");
+	
+}
